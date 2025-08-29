b2648c990c104f53e3516a8f03a6fd187a4b7870
Taha Hamim Boughanmi <99847753+tahahamim86@users.noreply.github.com>
2025-08-29 15:48:17 +0100

    Update PasswordEncoder.java

--- src/main/java/com/example/emailverificationbackend/security/PasswordEncoder.java
+++ src/main/java/com/example/emailverificationbackend/security/PasswordEncoder.java
@@ -7,8 +7,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 public class PasswordEncoder {
 
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+   @Bean
+public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder(6); // Use 8 instead of 10 or higher
+}
+
 }
